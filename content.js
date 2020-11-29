8caaf933dd72b2953326ddcd74049294d6ebf4f8
Maliya Naz <maliyanaz18@gmail.com>
2020-11-28 23:02:56 -0500

    experimenting with regular expression text replacement

--- content.js
+++ content.js
@@ -1,6 +1,23 @@
-var elements = document.getElementsByTagName('p');
+// var elements = document.getElementsByTagName('div');
+//
+// for (var i = 0; i < elements.length; i++) {
+//    var element = elements[i];
+//    //console.log(element);
+// //element.style.setProperty('border','solid 1px red');
+// var r = Math.floor(Math.random()*255);
+// var g = Math.floor(Math.random()*45);
+// var b = Math.floor(Math.random()*100);
+// var c = "rgb(" + r + ","  + r ","  + b ")";
+//
+// element.style.setProperty('background-color', c);
+//
+// }
 
-for (var i = 0; i < elements.length; i++) {
-   var element = elements[i];
-}
+//
+var html = document.querySelector('html');
+ var walker = document.createTreeWalker(html,NodeFilter.SHOW_TEXT);
 
+ var node;
+ while(node = walker.nextNode()) {
+  node.nodeValue = node.nodeValue.replace(/democracy/gi, "facism")
+ }
